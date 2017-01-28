aa4fef55845fd3d3900a07559a74bafbfab89447
Prasham Trivedi <prash2488@gmail.com>
2017-01-28 18:16:25 +0530

    Started Reading Response

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ restService.use(bodyParser.json());
 restService.post('/webhook',function(req,res){
     console.log('hook request');
     if(req.body){
-        console.log(req.body);
+        if(req.body.result){
+            if(req.body.result.parameters){
+                if(req.body.parameters.Tech){
+                    console.log("Tech Object is "+req.body.parameters.Tech)
+                }
+                if(req.body.parameters.Functionality){
+                    console.log("Functionality Object is "+req.body.parameters.Functionality)
+                }
+                if(req.body.parameters.Keyword){
+                    console.log("Keyword Object is "+req.body.parameters.Keyword)
+                }
+                
+            }
+        }
     }
 })
 
