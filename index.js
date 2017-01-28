eacb5565adf7c44f96a3281c8b3a1271e018415b
Prasham Trivedi <prash2488@gmail.com>
2017-01-28 19:15:32 +0530

    Read params from ENV

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ restService.post('/webhook',function(req,res){
     // oauth key/secret (to get a token)
     github.authenticate({
             type: "oauth",
-            key: config.githubClientId,
-            secret: config.githubSecret
+            key: process.env.CLIENTID || config.githubClientId,
+            secret: process.env.SECRET || config.githubSecret
     })
      var speech = 'empty speech';
      var topic = '';
