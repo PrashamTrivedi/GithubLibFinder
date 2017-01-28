8bddc8d9d36d73f2029609eea4f9eaf19dd9e74d
Prasham Trivedi <prash2488@gmail.com>
2017-01-28 18:20:03 +0530

    Preparing Speech and Responsing

--- index.js
+++ index.js
@@ -8,21 +8,38 @@ restService.use(bodyParser.json());
 
 restService.post('/webhook',function(req,res){
     console.log('hook request');
+     var speech = 'empty speech';
     if(req.body){
         if(req.body.result){
             if(req.body.result.parameters){
+                 if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
                 if(req.body.parameters.Tech){
                     console.log("Tech Object is "+req.body.parameters.Tech)
+                    speech +='Tech: '+req.body.parameters.Tech;
+                    speech += ' ';
                 }
                 if(req.body.parameters.Functionality){
                     console.log("Functionality Object is "+req.body.parameters.Functionality)
+                    speech +='Functionality: '+req.body.parameters.Functionality;
+                    speech += ' ';
                 }
                 if(req.body.parameters.Keyword){
                     console.log("Keyword Object is "+req.body.parameters.Keyword)
+                    speech +='Keywords: '+req.body.parameters.Keyword;
+                    speech += ' ';
                 }
                 
             }
         }
+          return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
+        });
     }
 })
 
